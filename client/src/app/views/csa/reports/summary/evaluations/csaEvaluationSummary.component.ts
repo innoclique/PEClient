4d353f209131fa61f22cab409aa28aa4eb9c622c
GSANGLI <govindmailid@gmail.com>
2020-12-13 14:17:21 +0530

    Add files via upload

---
+++ client/src/app/views/csa/reports/summary/evaluations/csaEvaluationSummary.component.ts
@@ -0,0 +1,114 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import "ag-grid-community";
+import { GridApi, GridOptions } from 'ag-grid-community';
+import { AuthService } from './../../../../../services/auth.service';
+import RefData from "../../../../psa/reports/data/refData";
+import ReportTemplates from '../../../../psa/reports/data/reports-templates';
+import { ReportsService } from '../../../../../services/reports.service';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './csaEvaluationSummary.component.html'
+})
+export class CSAEvaluationsSummary {
+  public gridOptions: GridOptions;
+  public showGrid: boolean;
+  public rowData: any[];
+  private api: GridApi;
+  detailCellRendererParams: any;
+  defaultColDef: any;
+  currentUser: any;
+  cscData: any = undefined;
+  currentOrganization: any;
+  detailCellRenderer: any;
+  frameworkComponents: any;
+  constructor(
+    public authService: AuthService,
+    public reportService: ReportsService,
+    public router: Router,
+    private activatedRoute: ActivatedRoute, ) {
+    this.currentUser = this.authService.getCurrentUser();
+    this.currentOrganization = this.authService.getOrganization();
+    this.gridOptions = <GridOptions>{};
+    this.gridOptions = {
+      columnDefs: this.getCSAEvaluationsSummaryColumnDefs(),
+    }
+    this.defaultColDef = ReportTemplates.defaultColDef;
+  }
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+    this.getEvaluationsSummary();
+  }
+
+  public headerHeightSetter() {
+    var padding = 20;
+    var height = ReportTemplates.headerHeightGetter() + padding;
+    this.api.setHeaderHeight(height);
+    this.api.resetRowHeights();
+}
+  getEvaluationsSummary() {
+    let { Organization, _id } = this.currentUser;
+    let orgId = Organization._id;
+
+    let reqBody: any = {
+      orgId: orgId,
+      reportType: 'EVALUATIONS_SUMMARY'
+    };
+    this.reportService.getReport(reqBody).subscribe(apiResponse => {
+      console.log('EVALUATIONS_SUMMARY : ', apiResponse);
+      this.createRowData(apiResponse);
+    });
+
+  }
+
+  getCSAEvaluationsSummaryColumnDefs() {
+    return [
+      { headerName: 'Evaluation Period', field: 'evaluationPeriod' },
+      { headerName: 'Type of Evaluations', field: 'evaluationsType' },
+      { headerName: '# of Evaluations', field: 'evaluationsCount' },
+    ];
+  }
+
+  createRowData(evaluationSummary: any) {
+    const rowData: any[] = [];
+    var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    rowData.push({
+      evaluationPeriod: ReportTemplates.months[this.currentOrganization.StartMonth] + "'" + ReportTemplates.getYear() + ' To ' + this.currentOrganization.EndMonth.substring(0, 3) + "'" + ReportTemplates.getYear(),
+      evaluationsType: RefData.evaluationTypes[0],
+      evaluationsCount: evaluationSummary.data,
+    });
+    this.rowData = rowData;
+  }
+
+  gotoDashboard() {
+    this.router.navigate(['/csa/dashboard'])
+  }
+
+  onBtExport() {
+    var params = {
+      columnWidth: parseFloat('200'),
+      sheetName: 'Client-Info',
+      exportMode: undefined,
+      suppressTextAsCDATA: false,
+      rowHeight: undefined,
+      headerRowHeight: undefined,
+    };
+    this.api.exportDataAsExcel(params);
+  }
+
+  onReady(params: any) {
+    this.api = params.api;
+    console.log('onReady');
+    this.api.sizeColumnsToFit();
+    this.gridOptions.rowHeight = 34;
+    this.gridOptions.groupMultiAutoColumn = true;
+    this.gridOptions.columnApi.setColumnVisible('isPastData', false);
+  }
+
+  onQuickFilterChanged($event: any) {
+    this.api.setQuickFilter($event.target.value);
+  }
+
+}
---
+++ client/src/app/views/csa/reports/summary/payment/csaPaymentSummary.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import "ag-grid-community";
+import { GridApi, GridOptions } from 'ag-grid-community';
+import { AuthService } from './../../../../../services/auth.service';
+import RefData from "../../../../psa/reports/data/refData";
+import ReportTemplates from '../../../../psa/reports/data/reports-templates';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './csaPaymentSummary.component.html'
+})
+export class CSAPaymentSummary {
+  public gridOptions: GridOptions;
+  public showGrid: boolean;
+  public rowData: any[];
+  private api: GridApi;
+  detailCellRendererParams: any;
+  defaultColDef: any;
+  currentUser: any;
+  cscData: any = undefined;
+  currentOrganization: any;
+  detailCellRenderer: any;
+  frameworkComponents: any;
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private activatedRoute: ActivatedRoute, ) {
+    this.currentUser = this.authService.getCurrentUser();
+    this.currentOrganization = this.authService.getOrganization();
+
+    // we pass an empty gridOptions in, so we can grab the api out
+    this.gridOptions = <GridOptions>{};
+    this.gridOptions = {
+      columnDefs: this.getCSAPaymentsSummaryColumnDefs(),
+    }
+    this.defaultColDef = ReportTemplates.defaultColDef;
+    this.createRowData();
+  }
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+  }
+
+  public headerHeightSetter() {
+    var padding = 20;
+    var height = ReportTemplates.headerHeightGetter() + padding;
+    this.api.setHeaderHeight(height);
+    this.api.resetRowHeights();
+  }
+  getCSAPaymentsSummaryColumnDefs() {
+    return ReportTemplates.csaPaymentsSummaryColumnDefs.columnDefs;
+  }
+
+  createRowData() {
+    const rowData: any[] = [];
+    // console.log('inside createHistoryData : ');
+    var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    for (let i = 0; i < 20; i++) {
+      rowData.push({
+        evaluationPeriod: "JAN'20-DEC'20",
+        purchasedOn: new Date(2010, 0, 1).toLocaleDateString(undefined, options),
+        evaluationsType: RefData.evaluationTypes[0],
+        licPurchasesCount: Math.round(Math.random() * 10),
+      });
+    }
+    this.rowData = rowData;
+  }
+
+  gotoDashboard() {
+    this.router.navigate(['/csa/dashboard'])
+  }
+
+  onBtExport() {
+    var params = {
+      columnWidth: parseFloat('200'),
+      sheetName: 'Client-Info',
+      exportMode: undefined,
+      suppressTextAsCDATA: false,
+      rowHeight: undefined,
+      headerRowHeight: undefined,
+    };
+    this.api.exportDataAsExcel(params);
+  }
+
+  onReady(params: any) {
+    this.api = params.api;
+    console.log('onReady');
+    this.api.sizeColumnsToFit();
+    this.gridOptions.rowHeight = 34;
+    this.gridOptions.groupMultiAutoColumn = true;
+    this.gridOptions.columnApi.setColumnVisible('isPastData', false);
+  }
+
+  onQuickFilterChanged($event: any) {
+    this.api.setQuickFilter($event.target.value);
+  }
+
+
+}
