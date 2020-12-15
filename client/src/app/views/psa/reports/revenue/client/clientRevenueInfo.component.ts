58de95a3b20ff37717d124b83090c25e5feba748
GSANGLI <govindmailid@gmail.com>
2020-12-15 13:37:26 +0530

    Update clientRevenueInfo.component.ts

--- client/src/app/views/psa/reports/revenue/client/clientRevenueInfo.component.ts
+++ client/src/app/views/psa/reports/revenue/client/clientRevenueInfo.component.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import "ag-grid-community";
 import { GridApi, GridOptions } from 'ag-grid-community';
 import { AuthService } from '../../../../../services/auth.service';
+import { ReportsService } from '../../../../../services/reports.service';
 import RefData from "../../data/refData";
 import ReportTemplates from '../../data/reports-templates';
-import { ReportsService } from '../../../../../services/reports.service';
 
 @Component({
     selector: 'my-app',
@@ -46,11 +46,12 @@ export class ClientRevenueInfoComponent {
         this.getClientsRevenue();
     }
 
-     headerHeightSetter(event) {
+    headerHeightSetter(event) {
         var padding = 20;
         var height = ReportTemplates.headerHeightGetter() + padding;
         this.api.setHeaderHeight(height);
         this.api.resetRowHeights();
+        this.api.sizeColumnsToFit();
     }
 
     getClientsRevenue() {
@@ -88,27 +89,9 @@ export class ClientRevenueInfoComponent {
                 licPurchasesCount: Math.round(Math.random() * 10),
                 purchasesCount: Math.round(Math.random() * 1000),
                 minPurchasedOn: new Date(2010, 0, 1).toLocaleDateString(undefined, options),
-                children: this.createHistoryData()
             });
         }
         this.rowData = rowData;
-    }
-
-    private createHistoryData() {
-        const rowData: any[] = [];
-        var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-
-        for (let i = 0; i < 20; i++) {
-            rowData.push({
-                purchasedOn: new Date(2010, 0, 1).toLocaleDateString(undefined, options),
-                purchaseCount: Math.round(Math.random() * 1000),
-                paymentTypes: RefData.paymentTypes[Math.random() < 0.5 ? 1 : 0],
-                usageType: RefData.usageTypes[Math.random() < 0.5 ? 1 : 0],
-                evaluationsType: RefData.evaluationTypes[0],
-                evaluationPeriod: "JAN'20-DEC'20",
-            });
-        }
-        return rowData;
     }
 
     getPsaClientRevenueInfoColumnDefs() {
@@ -117,12 +100,13 @@ export class ClientRevenueInfoComponent {
             { headerName: 'Active', field: 'active' },
             { headerName: 'Usage Type', field: 'usageType' },
             { headerName: 'Type of Evaluations', field: 'evaluationsType' },
-            { headerName: 'Revenue (all years)', field: 'purchasesCount' },
-            { headerName: 'Payment Type (Monthly/Yearly)', field: 'paymentTypes' },
-            { headerName: "Actions", suppressSizeToFit: true, filter: false, sorting: false, onCellClicked: this.gotoClientRevenueDetails.bind(this),
+            { headerName: 'Revenue (CAD)', field: 'purchasesCount', type: 'rightAligned', valueFormatter: params => params.data.purchasesCount.toFixed(2) },
+            { headerName: 'Payment Type', field: 'paymentTypes' },
+            {
+                headerName: "Actions", suppressSizeToFit: true, filter: false, sorting: false, onCellClicked: this.gotoClientRevenueDetails.bind(this),
                 cellRenderer: () => {
                     return `  <i class="fa fa-bars"   style="cursor:pointer ;padding: 7px 10px 0 0;
-            font-size: 17px; color:blue"   title="view Client Revenue Details" > Revenue Details</i>`
+            font-size: 17px; "   title="view Client Revenue Details" ></i>`
                 }
             }
         ];
@@ -155,7 +139,7 @@ export class ClientRevenueInfoComponent {
         this.api = params.api;
         console.log('onReady');
         this.api.sizeColumnsToFit();
-        this.gridOptions.rowHeight = 34;
+        this.gridOptions.rowHeight = 40;
     }
 
     onQuickFilterChanged($event: any) {
