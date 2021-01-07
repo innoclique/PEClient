7f64292d17a278e9744b78c58b3e79514ffaf73c
GSANGLI <govindmailid@gmail.com>
2021-01-07 22:48:26 +0530

    Update csaEvaluationSummary.component.ts

--- client/src/app/views/csa/reports/summary/evaluations/csaEvaluationSummary.component.ts
+++ client/src/app/views/csa/reports/summary/evaluations/csaEvaluationSummary.component.ts
@@ -77,7 +77,8 @@ export class CSAEvaluationsSummary {
     const rowData: any[] = [];
     var options = { year: 'numeric', month: '2-digit', day: '2-digit' };
     rowData.push({
-      evaluationPeriod: ReportTemplates.months[this.currentOrganization.StartMonth] + "'" + ReportTemplates.getYear() + ' To ' + ReportTemplates.months[ReportTemplates.months.indexOf(this.currentOrganization.EndMonth.substring(0, 3))+1] + "'" + ReportTemplates.getYear(),
+      evaluationPeriod: "Jan'21 To Dec'21",
+//       evaluationPeriod: ReportTemplates.months[this.currentOrganization.StartMonth] + "'" + ReportTemplates.getYear() + ' To ' + ReportTemplates.months[ReportTemplates.months.indexOf(this.currentOrganization.EndMonth.substring(0, 3))+1] + "'" + ReportTemplates.getYear(),
       evaluationsType: RefData.evaluationTypes[0],
       evaluationsCount: evaluationSummary.data,
     });
