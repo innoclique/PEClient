a8d2ab4c92541a5b2a498a86cdc8a942d872d50c
GSANGLI <govindmailid@gmail.com>
2021-02-16 14:56:29 +0530

    user type updated to usage type

--- client/src/app/views/psa/reports/info/reseller-clients/resellerClientsInfo.component.ts
+++ client/src/app/views/psa/reports/info/reseller-clients/resellerClientsInfo.component.ts
@@ -88,7 +88,7 @@ export class ResellerClientInfoComponent {
         var employeesCount = 0;
         if (paymentReleases && paymentReleases.length > 0) {
             for (let payment of paymentReleases) {
-                if (payment.UserType === 'License') {
+                if (payment.UsageType === 'License') {
                     if (payment.Type != 'Adhoc') {
                         licencesCount++;
                     } else {
